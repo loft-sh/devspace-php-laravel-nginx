756c504541274f95e4454868877d31fb3187b8b5
Levent Ogut <levent@ogut.me>
2021-05-03 12:32:05 +0300

    fix update_db_user command and remove ping/pong example

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('ping', function () {
-    return "pong";
-});
+
 
 Route::get('info', function () {
     return phpinfo();
